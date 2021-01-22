b0094801fc147506a7cf5046023743c15f618e65
tigerfly <18356285335@163.com>
2021-01-22 19:57:24 +0800

    log

--- UIKit_Practice/UserInterface/AnimationAndHaptics.m
+++ UIKit_Practice/UserInterface/AnimationAndHaptics.m
@@ -81,7 +81,7 @@
     /**
      Animation Delegate
      
-     - UIViewControllerTransitioningDelegate
+     ● UIViewControllerTransitioningDelegate
      A set of methods that vend objects used to manage a
      fixed-length or interactive transition between view controllers.
      
@@ -124,7 +124,7 @@
     /**
      Non-Interactive Transitions
      
-     - UIViewControllerAnimatedTransitioning
+     ● UIViewControllerAnimatedTransitioning
      A set of methods for implementing the animations for a custom view
      controller transition.
      
@@ -163,49 +163,215 @@
      more on defining interactive transitions, see
      UIViewControllerInteractiveTransitioning.
      
-     - UIViewControllerContextTransitioning
+     
+     ● UIViewControllerContextTransitioning
      A set of methods that provide contextual information for transition
      animations between view controllers.
+     
+     Do not adopt this protocol in your own classes, nor should you
+     directly create objects that adopt this protocol. During a
+     transition, the animator objects involved in that transition
+     receive a fully configured context object from UIKit. Custom
+     animator objects—objects that adopt the
+     UIViewControllerAnimatedTransitioning or
+     UIViewControllerInteractiveTransitioning protocol—should simply
+     retrieve the information they need from the provided object.
+     
+     A context object encapsulates information about the views and
+     view controllers involved in the transition. It also contains
+     details about the how to execute the transition. For interactive
+     transitions, the interactive animator object uses the methods
+     of this protocol to report the animation’s progress. When the
+     animation starts, the interactive animator object must save a
+     pointer to the context object. Based on user interactions,
+     the animator object then calls the updateInteractiveTransition:,
+     finishInteractiveTransition, or cancelInteractiveTransition
+     methods to report the progress toward completing the animation.
+     Those methods send that information to UIKit so that it can
+     drive the timing of the animations.
+     
+     ⚠️ When defining custom animator objects, always check the
+     value returned by the isAnimated method to determine whether
+     you should create animations at all. And when you do create
+     transition animations, always call the completeTransition:
+     method from an appropriate completion block to let UIKit know
+     when all of your animations have finished.
      */
+    
     
     /**
      Interactive Transitions
 
-     - UIPercentDrivenInteractiveTransition
+     
+     ● UIPercentDrivenInteractiveTransition
      An object that drives an interactive animation between one view
      controller and another.
      
-     - UIViewControllerInteractiveTransitioning
+     A percent-driven interactive transition object relies on a
+     transition animator delegate—a custom object that adopts the
+     UIViewControllerAnimatedTransitioning protocol—to set up and
+     perform the animations.
+     
+     To use this concrete class, return an instance of it from
+     your view controller delegate when asked for an interactive
+     transition controller. As user events arrive that would
+     affect the progress of a transition, call the
+     updateInteractiveTransition:, cancelInteractiveTransition,
+     and finishInteractiveTransition methods to reflect the
+     current progress. For example, you might call these methods
+     from a gesture recognizer to reflect how much of the gesture
+     is completed.
+     
+     You can subclass UIPercentDrivenInteractiveTransition, but
+     if you do so you must start each of your method overrides
+     with a call to the super implementation of the method.
+     
+     
+     ● UIViewControllerInteractiveTransitioning
      A set of methods that enable an object (such as a navigation
      controller) to drive a view controller transition.
      
-     - UIViewImplicitlyAnimating
+     An interactive transition delegate (which is the term for
+     an object that supports this protocol) can respond to touch
+     events, or to time-varying programmatic input, by speeding
+     up, slowing down, or even reversing the progress of a view
+     controller transition. For example, an interactive transition
+     on a navigation controller could respond to a swipe gesture
+     by moving a view controller onto or off of the navigation stack.
+     
+     To support an interactive view controller transition, you
+     must also provide a transition animator delegate, which
+     is a custom object that adopts the
+     UIViewControllerAnimatedTransitioning protocol. The
+     transition delegate and the transition animator can, if
+     you wish, be defined within a single custom class, but
+     the class must adopt both protocols.
+     
+     If you instead want to provide a fixed-duration animated
+     view controller transition—one that does not support user
+     interaction—use a transition animator delegate on its own.
+     Refer to UIViewControllerAnimatedTransitioning.
+     
+     For the methods you can call to retrieve view transition
+     context information from within your
+     startInteractiveTransition: method, refer to
+     UIViewControllerContextTransitioning.
+     
+     
+     ● UIViewImplicitlyAnimating
      An interface for modifying an animation while it is running.
+     
+     Animator objects used in interruptible view controller
+     transitions adopt the UIViewImplicitlyAnimating protocol
+     to modify in-flight transition animations. This protocol
+     also conforms to the UIViewAnimating protocol, which
+     specifies methods for starting and stopping animations
+     and for updating their state.
+     
+     The UIViewPropertyAnimator class adopts this protocol and
+     implements all of its methods. You can adopt this protocol
+     in your own classes to implement custom animator objects.
+     When adopting this protocol, it is recommended that you
+     implement all of the methods.
      */
+    
     
     /**
      Transition Coordinators
 
-     - UIViewControllerTransitionCoordinator
+     ● UIViewControllerTransitionCoordinator
      A set of methods that provides support for animations associated
      with a view controller transition.
      
-     - UIViewControllerTransitionCoordinatorContext
+     Typically, you do not adopt this protocol in your own classes.
+     When you present or dismiss a view controller, UIKit creates
+     a transition coordinator object automatically and assigns it
+     to the view controller’s transitionCoordinator property.
+     That transition coordinator object is ephemeral and lasts
+     for the duration of the transition animation.
+     
+     You can use a transition coordinator object to perform
+     tasks that are related to a transition but that are separate
+     from what the animator objects are doing. During a transition,
+     the animator objects are responsible for putting the new
+     view controller content onscreen, but there may be other
+     visual elements that need to be displayed too. For example,
+     a presentation controller might want to animate the
+     appearance or disappearance of decoration views that are
+     separate from the view controller content. In that case, it
+     uses the transition coordinator to perform those animations.
+     
+     The transition coordinator works with the animator objects
+     to ensure that any extra animations are performed in the
+     same animation group as the transition animations. Having
+     the animations in the same group means that they execute at
+     the same time and can all respond to timing changes
+     provided by an interactive animator object. These timing
+     adjustments happen automatically and do not require any
+     extra code on your part.
+     
+     Using the transition coordinator to handle view hierarchy
+     animations is preferred over making those same changes in
+     the viewWillAppear: or similar methods of your view
+     controllers. The blocks you register with the methods of
+     this protocol are guaranteed to execute at the same time
+     as the transition animations. More importantly, the
+     transition coordinator provides important information
+     about the state of the transition, such as whether it was
+     cancelled, to your animation blocks through the
+     UIViewControllerTransitionCoordinatorContext object.
+     
+     In addition to registering animations to perform during
+     the transition, you can call the
+     notifyWhenInteractionEndsUsingBlock: method to register
+     a block to clean up animations associated with an
+     interactive transition. Cleaning up is particularly
+     important when the user cancels a transition interactively.
+     When a cancellation occurs, you need to return to the
+     original view hierarchy state as it existed before the
+     transition.
+     
+     Because the transition coordinator is in effect only
+     during a transition, UIKit releases the object when the
+     transition finishes and the final callback is made.
+     
+     
+     ● UIViewControllerTransitionCoordinatorContext
      A set of methods that provides information about an in-progress
      view controller transition.
+     
+     Do not adopt this protocol in your own classes. UIKit
+     creates an object that adopts this protocol and makes
+     it available to your code when you animate changes
+     using a transition coordinator object.
+     
+     A transition coordinator context provides most of the
+     same information as an object that adopts the
+     UIViewControllerContextTransitioning protocol. You use
+     this contextual information to determine the animation
+     parameters, such as the view in which the animations
+     take place, whether the transition is interactive, or
+     whether the transition was the result of an interface
+     orientation change. You then apply that information to
+     the animations you create.
+     
+     Most animations take place in the view returned by the
+     containerView method. And at the time your animation
+     blocks are executed, the view hierarchy already contains
+     the view of the from view controller. You can use your
+     animation blocks to animate additional content in that
+     same container view or you can animate content in an
+     entirely different view.
      */
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 100, 100, 20)];
-    label.text = @"fasdfasfasdf";
-    label.font = [UIFont fontWithName:@"PingFangSC-Regular" size:12];
-    label.textColor = [UIColor redColor];
-    label.highlightedTextColor = [UIColor yellowColor];
-    label.highlighted = YES;
-    [self.view addSubview:label];
     
-//    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:@"shfasdfasdfdas"];
-//    [string addAttributes:@{NSFontAttributeName:[UIFont fontWithName:@"PingFangSC-Regular" size:12],NSForegroundColorAttributeName:[UIColor redColor]} range:NSMakeRange(0, string.length)];
-//    label.attributedText = string;
+
+#pragma mark -- Physics-Based Animations
+    
+    
+    
+
+    
     
 }
 
