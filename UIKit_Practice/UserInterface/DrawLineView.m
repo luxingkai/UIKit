d5a018a2031885db06324f4749f815b41a2dd857
luxingkai <luxingkai@sungrowpower.com>
2023-05-29 19:05:15 +0800

    comment scale label

--- UIKit_Practice/UserInterface/DrawLineView.m
+++ UIKit_Practice/UserInterface/DrawLineView.m
@@ -35,83 +35,91 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         
-        //        UITapGestureRecognizer
-        //        UISwipeGestureRecognizer
-        //        UIPanGestureRecognizer
-        //        UIScreenEdgePanGestureRecognizer
-        //        UILongPressGestureRecognizer
-        
+        /// 最左边滑块
         self.l_slideView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 30, 18, 22)];
         self.l_slideView.image = [UIImage imageNamed:@"os_slider"];
         self.l_slideView.userInteractionEnabled = true;
         [self addSubview:self.l_slideView];
-        
-        UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
-        [self.l_slideView addGestureRecognizer:panGesture];
-        
+        /// 最右边滑块
         self.r_slideView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 30, 18, 22)];
         self.r_slideView.image = [UIImage imageNamed:@"os_slider"];
         self.r_slideView.userInteractionEnabled = true;
         [self addSubview:self.r_slideView];
+        
+        UIPanGestureRecognizer *minPanGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(minPan:)];
+        [self.l_slideView addGestureRecognizer:minPanGesture];
+        
+        UIPanGestureRecognizer *maxPanGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(maxPan:)];
+        [self.r_slideView addGestureRecognizer:maxPanGesture];
+        
     }
     return self;
 }
 
-//- (void)drawRect:(CGRect)rect {
-//    [super drawRect:rect];
-//    
-//    /// 刻度宽度
-//    float scale_line_w = 1;
-//    /// 刻度高度
-//    float scale_line_h = 4;
-//    /// 刻度间距
-//    float scale_inset_w = (self.bounds.size.width - self.range.count * scale_line_w) / (self.range.count - 1);
-//    /// 主轴
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//    
-//    CGMutablePathRef axisPath = CGPathCreateMutable();
-//    CGPathMoveToPoint(axisPath, NULL, 0, AXIS_Y);
-//    CGPathAddLineToPoint(axisPath, NULL, rect.size.width, AXIS_Y);
-//    CGPathCloseSubpath(axisPath);
-//    CGContextAddPath(context, axisPath);
-//    
-//    CGContextSetFillColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
-//    CGContextSetStrokeColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
-//    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-//    CGContextSetFillColorSpace(context, colorSpaceRef);
-//    CGContextSetLineWidth(context, 4);
-//    
-//    CGContextStrokePath(context);
-//    
-//    CGPathRelease(axisPath);
-//    
-//    /// 刻度
-//    for (int i = 0; i <= self.range.count; i ++) {
-//        
-//        CGMutablePathRef scalePath = CGPathCreateMutable();
-//        CGPathMoveToPoint(scalePath, NULL, i * (scale_line_w + scale_inset_w), 14);
-//        CGPathAddLineToPoint(scalePath, NULL, i * (scale_line_w + scale_inset_w), 18);
-//        CGPathCloseSubpath(scalePath);
-//        CGContextAddPath(context, scalePath);
-//        
-//        CGContextSetFillColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
-//        CGContextSetStrokeColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
-//        CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-//        CGContextSetFillColorSpace(context, colorSpaceRef);
-//        CGContextSetLineWidth(context, 1);
-//
-//        CGContextStrokePath(context);
-//        
-//        CGPathRelease(scalePath);
-//        
-//        /// 每个刻度对应的数值
-//        CATextLayer *textLayer = [CATextLayer layer];
-////        textLayer.frame = CGRectMake(, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
-//    }
-//    
-//}
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
+    
+    /// 刻度宽度
+    float scale_line_w = 1;
+    /// 刻度高度
+    float scale_line_h = 4;
+    /// 刻度间距
+    float scale_inset_w = (self.bounds.size.width - self.range.count * scale_line_w) / (self.range.count - 1);
+    /// 主轴
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGMutablePathRef axisPath = CGPathCreateMutable();
+    CGPathMoveToPoint(axisPath, NULL, 0, AXIS_Y);
+    CGPathAddLineToPoint(axisPath, NULL, rect.size.width, AXIS_Y);
+    CGPathCloseSubpath(axisPath);
+    CGContextAddPath(context, axisPath);
+    
+    CGContextSetFillColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
+    CGContextSetStrokeColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+    CGContextSetFillColorSpace(context, colorSpaceRef);
+    CGContextSetLineWidth(context, scale_line_h);
+    
+    CGContextStrokePath(context);
+    
+    CGPathRelease(axisPath);
+    
+    /// 刻度
+    for (int i = 0; i <= self.range.count; i ++) {
+        
+        CGMutablePathRef scalePath = CGPathCreateMutable();
+        CGPathMoveToPoint(scalePath, NULL, i * (scale_line_w + scale_inset_w), 14);
+        CGPathAddLineToPoint(scalePath, NULL, i * (scale_line_w + scale_inset_w), 18);
+        CGPathCloseSubpath(scalePath);
+        CGContextAddPath(context, scalePath);
+        
+        CGContextSetFillColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
+        CGContextSetStrokeColorWithColor(context, [self colorHex:0xE8E8E8].CGColor);
+        CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+        CGContextSetFillColorSpace(context, colorSpaceRef);
+        CGContextSetLineWidth(context, 1);
+        
+        CGContextStrokePath(context);
+        
+        CGPathRelease(scalePath);
+        
+        /// 每个刻度对应的数值
+        if (i == self.range.count) return;
+        NSString *singleScala = [NSString stringWithFormat:@"%@",self.range[i]];
+        CGFloat scale_text_w = [self widthForScaleValue:singleScala];
+        CATextLayer *textLayer = [CATextLayer layer];
+        textLayer.frame = CGRectMake(i * (scale_line_w + scale_inset_w), 0, scale_text_w, 14);
+        textLayer.string = singleScala;
+        textLayer.font = CFBridgingRetain([UIFont systemFontOfSize:10 weight:UIFontWeightMedium]);
+        textLayer.fontSize = 10;
+        textLayer.foregroundColor = [self colorHex:0x999999].CGColor;
+        textLayer.contentsScale = [UIScreen mainScreen].scale;
+        [self.layer addSublayer:textLayer];
+    }
+    
+}
 
-- (void)pan:(UIPanGestureRecognizer *)gesture {
+- (void)minPan:(UIPanGestureRecognizer *)gesture {
     if (gesture.view == nil) return;
     UIView *piece = gesture.view;
     // Get the changes in the X and Y directions relative to
@@ -125,26 +133,29 @@
     // Update the position for the .began, .changed, and .ended states
     if (gesture.state != UIGestureRecognizerStateCancelled) {
         /// 超出视图范围不准滑动
-        NSLog(@"%@",NSStringFromCGPoint(piece.center));
-        if (piece.center.x < 9 || piece.center.x > (self.frame.size.width - 9)) {
-            return;
-        }
-        // Add the X and Y translation to the view's original position.
-        float pow_c = powf(translation.y, 2.0) + powf(translation.x, 2.0);
-        float c = sqrtf(pow_c);
-        float arc = asinf(translation.y / c);
-        NSLog(@"%f",arc);
-        float limit = M_PI / 6;
-        ///
-        if (-limit <= arc && arc <= limit) {
-            CGPoint newCenter = CGPointMake(self.initialCenter.x + translation.x, self.initialCenter.y);
-            piece.center = newCenter;
-        }
+        NSUInteger integer_part = ceil(piece.center.x);
+        printf("%lu\n",(unsigned long)integer_part);
+//        if (integer_part >= 9 && integer_part <= (self.frame.size.width - 9)) {
+            // Add the X and Y translation to the view's original position.
+            float pow_c = powf(translation.y, 2.0) + powf(translation.x, 2.0);
+            float c = sqrtf(pow_c);
+            float arc = asinf(translation.y / c);
+            float limit = M_PI / 6;
+            ///
+            if (-limit <= arc && arc <= limit) {
+                CGPoint newCenter = CGPointMake(self.initialCenter.x + translation.x, self.initialCenter.y);
+                piece.center = newCenter;
+            }
+//        }
+        
     } else {
         // On cancellation, return the piece to its original location.
         piece.center = self.initialCenter;
     }
     
+}
+
+- (void)maxPan:(UIPanGestureRecognizer *)gesture {
     
 }
 
@@ -157,10 +168,10 @@
     return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1];
 }
 
-//- (CGFloat)widthForScaleValue:(NSString *)scale {
-//
-//
-//}
+- (CGFloat)widthForScaleValue:(NSString *)scale {
+    CGRect rect = [scale boundingRectWithSize:CGSizeMake(MAXFLOAT, 14) options:NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:10 weight:UIFontWeightMedium]} context:nil];
+    return ceil(rect.size.width);
+}
 
 - (void)dealloc {
     
